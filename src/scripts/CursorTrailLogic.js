86f51ffecd38e6c72f2c039826c56ddc3bee311c
PaoloMar <PaoloMar_@hotmail.com>
2026-05-07 20:16:31 -0600

    Añadir pequeños detalles y "completo" la sección sobre mí(?

---
+++ src/scripts/CursorTrailLogic.js
@@ -0,0 +1,35 @@
+    const coords = { x: 0, y: 0 }
+    const circles = document.querySelectorAll(".circle");
+
+    circles.forEach((circle) => {
+        circle.x = 0;
+        circle.y = 0
+    });
+
+    window.addEventListener("mousemove", (e) => {
+        coords.x = e.clientX
+        coords.y = e.clientY
+    });
+
+    function animateCircles(){
+        let x = coords.x;
+        let y = coords.y;
+
+        circles.forEach((circle, index) => {
+            circle.style.left = x - 7 + "px";
+            circle.style.top = y - 7 + "px";
+
+            circle.style.scale = (circles.length - index) / circles.length;
+
+            circle.x = x;
+            circle.y = y;
+
+            const nextCircle = circles[index + 1] || circles[0];
+            x += (nextCircle.x - x) * 0.5;
+            y += (nextCircle.y - y) * 0.5;
+        });
+
+       requestAnimationFrame(animateCircles) 
+    }
+
+    animateCircles();
